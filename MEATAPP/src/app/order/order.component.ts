233874d058a10c688e34ebd71ee28867c400d4f6
rdfenix <rhoodsk8erboy@gmail.com>
2019-05-03 14:09:47 -0300

    error

--- MEATAPP/src/app/order/order.component.ts
+++ MEATAPP/src/app/order/order.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RadioOption } from 'app/shared/radio/radio-option.model';
+import { OrderService } from './order.service';
+import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item';
 
 @Component({
   selector: 'mt-order',
@@ -14,9 +16,15 @@ export class OrderComponent implements OnInit {
     { label: "Cartão de Debito", value: "DEB" },
   ]
 
-  constructor() { }
+  constructor(private orderService = OrderService) { }
 
   ngOnInit() {
   }
+
+  cardItems(){
+    return this.orderService.cartItems()
+  }
+
+  increaseQty(item: CartItem){}
 
 }
